3381bc3a2f7c77623a6cea4fa36932f277f5e559
David Edmonds <davide@nutmeg.com>
2018-04-25 12:22:45 +0100

    Complete java implementation.

--- java/src/main/java/com/github/davidedmonds/Main.java
+++ java/src/main/java/com/github/davidedmonds/Main.java
@@ -1,7 +1,23 @@
 package com.github.davidedmonds;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import java.nio.charset.Charset;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.StreamSupport;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Testing");
+    public static void main(String[] args) throws Exception {
+        final String json = IOUtils.toString(Main.class.getClassLoader().getResource("cheeses.json"), Charset.forName("UTF-8"));
+        final Gson gson = new Gson();
+        final JsonArray array = gson.fromJson(json, JsonArray.class);
+        StreamSupport.stream(Spliterators.spliteratorUnknownSize(array.iterator(), Spliterator.IMMUTABLE), false)
+                .map(cheese -> cheese.getAsJsonObject().get("itemLabel").getAsString())
+                .filter(cheese -> !cheese.matches("^Q\\d+"))
+                .sorted()
+                .forEach(System.out::println);
     }
 }
