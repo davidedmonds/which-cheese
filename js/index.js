cd2a31dc661beed2f1b4e8c683b83d13b7834f88
David Edmonds <davide@nutmeg.com>
2018-04-25 12:31:23 +0100

    Implement JS cheese listing program.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-console.log("Testing");
+const cheeseList = require('cheese-list');
+const cheeseTest = RegExp('^Q\\d+');
+cheeseList.map(cheese => cheese.itemLabel)
+    .filter(cheese => !cheeseTest.test(cheese))
+    .sort()
+    .forEach(cheese => console.log(cheese));
